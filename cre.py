a100de2b9d1cc554d0c202321720c0aef597ddd7
Yuvaraja M <yuva200324@gmail.com>
2026-02-23 11:20:14 +0530

    Initialize crew with agents and tasks for blogging

---
+++ cre.py
@@ -0,0 +1,20 @@
+from crewai import Crew,Process
+from agents import blog_researcher, blog_writer
+from tasks import research_task,write_task
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+crew = Crew(
+    agents = [blog_researcher,blog_writer],
+    tasks=[research_task,write_task],
+    process = Process.sequential,
+    memory = True,
+    cache = True,
+    max_rpm = 100,
+    share_crew = True
+)
+
+result = crew.kickoff(inputs={'topic':'AI VS ML VS DL VS data science'})
+print(result)
